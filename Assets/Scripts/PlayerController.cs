24e1c204598bc66d59a4d604c5814bb52471615a
Shubham Rao <cshubhamrao@gmail.com>
2014-03-20 20:16:03 +0530

    Tweak speed to make game playable

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,12 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+	public float speed;
 	// Update is called once per frame
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		// float moveHorizontal = Input.GetAxis ("Horizontal");
-		rigidbody.AddForce(new Vector3(moveHorizontal,0.0f,moveVertical)*3);
+		rigidbody.AddForce(new Vector3(moveHorizontal,0.0f,moveVertical)* speed * Time.deltaTime);
 	}
 }
