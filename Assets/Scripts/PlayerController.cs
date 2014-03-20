929d1166928dc89fe80cfe43835f10ba3ab402b9
Shubham Rao <cshubhamrao@gmail.com>
2014-03-20 20:02:04 +0530

    Add code to aid movement of player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		// float moveHorizontal = Input.GetAxis ("Horizontal");
+		rigidbody.AddForce(new Vector3(moveHorizontal,0.0f,moveVertical)*3);
 	}
 }
